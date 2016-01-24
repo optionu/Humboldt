3fa46d8bb6a0055a9019c798bcf8ff992cb18c0d
Claus Höfele <claus@claushoefele.com>
2016-01-24 22:37:54 +0100

    Implement CollectionType

--- Sources/PositionCollection.swift
+++ Sources/PositionCollection.swift
@@ -37,3 +37,24 @@ extension PositionCollection : SequenceType {
         }
     }
 }
+
+extension PositionCollection : CollectionType {
+    public typealias Index = Int
+    
+    public var startIndex: Int {
+        return 0
+    }
+    
+    public var endIndex: Int {
+        return Int(OGR_G_GetPointCount(self.geometryStorage.geometry))
+    }
+    
+    public subscript(index: Int) -> Position {
+        let is3D = OGR_G_GetCoordinateDimension(self.geometryStorage.geometry) > 2
+        let x = OGR_G_GetX(self.geometryStorage.geometry, Int32(index))
+        let y = OGR_G_GetY(self.geometryStorage.geometry, Int32(index))
+        let altitude: Double? = is3D ? OGR_G_GetZ(self.geometryStorage.geometry, Int32(index)) : nil
+        
+        return Position(x: x, y: y, altitude: altitude)
+    }
+}
--- Tests/PositionCollectionTests.swift
+++ Tests/PositionCollectionTests.swift
@@ -30,4 +30,24 @@ class PositionCollectionTests: XCTestCase {
         }
         XCTAssertEqual(numPositions, positions.count)
     }
+    
+    func testSubscript() {
+        let positions = [Position(), Position(x: 1, y: 2), Position(x: 3, y: 4)]
+        
+        let geometry = OGR_G_CreateGeometry(wkbLineString)
+        OGR_G_AddPoint_2D(geometry, positions[0].x, positions[0].y)
+        OGR_G_AddPoint_2D(geometry, positions[1].x, positions[1].y)
+        OGR_G_AddPoint_2D(geometry, positions[2].x, positions[2].y)
+        let geometryStorage = GeometryStorage(geometry: geometry)!
+        
+        let positionsInCollection = PositionCollection(geometryStorage: geometryStorage)
+        XCTAssertEqual(positionsInCollection.count, positions.count)
+        XCTAssertEqual(positionsInCollection.startIndex, 0)
+        XCTAssertEqual(positionsInCollection.endIndex, positions.count)
+        
+        for index in 0..<positionsInCollection.count {
+            XCTAssertEqual(positionsInCollection[index].x, positions[index].x)
+            XCTAssertEqual(positionsInCollection[index].y, positions[index].y)
+        }
+    }
 }
